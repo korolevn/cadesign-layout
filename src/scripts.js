f00f6d433049566d0328af763386f0d7ac4ea96a
nkorolev <orevdrol@mail.ru>
2023-10-21 18:52:00 +0300

    - add slider script

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,8 @@
+import "./common-scripts/slider";
+import "./common-scripts/lazy-loading";
 import "./components/main-section/toggle";
-import "./components/features/slider";
 import "./components/modal/modal";
 import "./components/modal/validation";
 import "./components/contacts/map";
 import "./components/video-section/player";
-import "./common-scripts/lazy-loading";
 import "./components/vacancies/switch";
